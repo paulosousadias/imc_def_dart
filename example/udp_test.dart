c630c630aa30f0d4f040cb0dfcdc84766f88a9d3
Paulo Dias <paulo.sousa.dias@gmail.com>
2019-02-04 20:46:12 +0000

    Added missing src files header.

--- example/udp_test.dart
+++ example/udp_test.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2019, Paulo Sousa Dias. Please see the AUTHORS file for details.
+// All rights reserved. Use of this source code is governed by a Apache License v2.0 
+// license that can be found in the LICENSE file.
+
 import 'dart:async';
 import 'dart:io';
 import 'dart:typed_data';
