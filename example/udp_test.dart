444ccbaef60f1d8e04220ff3e544eac25146fcb4
Paulo Dias <paulo.sousa.dias@gmail.com>
2019-02-08 19:41:07 +0000

    Changed licence to BSD 3-Clause "New" or "Revised" License.

--- example/udp_test.dart
+++ example/udp_test.dart
@@ -1,5 +1,5 @@
 // Copyright (c) 2019, Paulo Sousa Dias. Please see the AUTHORS file for details.
-// All rights reserved. Use of this source code is governed by a Apache License v2.0 
+// All rights reserved. Use of this source code is governed by a BSD-style 
 // license that can be found in the LICENSE file.
 
 import 'dart:async';
