87fc6238191e790632e9646e1fee030701c4ac5d
Paulo Dias <paulo.sousa.dias@gmail.com>
2021-03-23 20:28:50 +0000

    Updated copy years.

--- example/example.dart
+++ example/example.dart
@@ -1,5 +1,5 @@
-// Copyright (c) 2019-20, Paulo Sousa Dias. Please see the AUTHORS file for details.
-// All rights reserved. Use of this source code is governed by a BSD-style 
+// Copyright (c) 2019-21, Paulo Sousa Dias. Please see the AUTHORS file for details.
+// All rights reserved. Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 import 'dart:async';
